a39181be497da9e10cd5a511d1e22152f526512d
Екатерина Каричева <yek.cari4ewa@yandex.ru>
2023-02-07 22:49:54 +0300

    Update tasks.py

--- app/report/tasks.py
+++ app/report/tasks.py
@@ -12,10 +12,31 @@ def write_report(menus):
 
     workbook = xlsxwriter.Workbook(f"app/data/{name}.xlsx")
     worksheet = workbook.add_worksheet()
-
+    
     set_format = workbook.add_format()
     set_format.set_bold()
     set_format.set_font_size(12)
+    set_format.set_text_wrap()
+    set_format.set_align('vjustify')
+    set_format.set_font_name('Times New Roman')
+
+    dish_format = workbook.add_format()
+    dish_format.set_bold()
+    dish_format.set_italic()
+    dish_format.set_font_size(10)
+    dish_format.set_text_wrap()
+    dish_format.set_align('vjustify')
+    dish_format.set_font_name('Times New Roman')
+
+    dish_title_format = workbook.add_format()
+    dish_title_format.set_bold()
+    dish_title_format.set_italic()
+    dish_title_format.set_font_size(12)
+    dish_title_format.set_text_wrap()
+    dish_title_format.set_align('vjustify')
+    dish_title_format.set_font_name('Times New Roman')
+
+    worksheet.set_column(1, 4, 40, set_format)
 
     menu_row = 0
     menu_count = 1
@@ -28,23 +49,23 @@ def write_report(menus):
     dish_count = 1
 
     for menu in menus:
-        worksheet.write(menu_row, 0, menu_count, set_format)
-        worksheet.write(menu_row, 1, menu["title"], set_format)
-        worksheet.write(menu_row, 2, menu["description"], set_format)
+        worksheet.write(menu_row, 0, menu_count)
+        worksheet.write(menu_row, 1, menu["title"])
+        worksheet.write(menu_row, 2, menu["description"])
         menu_count += 1
 
         for submenu in menu["submenus"]:
-            worksheet.write(submenu_row, 1, submenu_count, set_format)
-            worksheet.write(submenu_row, 2, submenu["title"], set_format)
-            worksheet.write(submenu_row, 3, submenu["description"], set_format)
+            worksheet.write(submenu_row, 1, submenu_count)
+            worksheet.write(submenu_row, 2, submenu["title"])
+            worksheet.write(submenu_row, 3, submenu["description"])
             submenu_row += len(submenu["dishes"]) + 1
             submenu_count += 1
             menu_gap += len(submenu["dishes"])
 
             for dish in submenu["dishes"]:
-                worksheet.write(dish_row, 2, dish_count, set_format)
-                worksheet.write(dish_row, 3, dish["title"], set_format)
-                worksheet.write(dish_row, 4, dish["description"], set_format)
+                worksheet.write(dish_row, 2, dish_count, dish_title_format)
+                worksheet.write(dish_row, 3, dish["title"], dish_title_format)
+                worksheet.write(dish_row, 4, dish["description"], dish_format)
                 worksheet.write(dish_row, 5, dish["price"], set_format)
                 dish_row += 1
                 dish_count += 1
@@ -58,3 +79,4 @@ def write_report(menus):
         dish_count = 1
 
     workbook.close()
+
